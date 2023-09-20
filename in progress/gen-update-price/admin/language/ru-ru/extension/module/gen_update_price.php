4f44de759f0d240aa53c5a545531c04a80798d62
Evgen Romanko <front5.sofona@gmail.com>
2023-09-20 16:15:38 +0300

    edit module gen-update-price

--- in progress/gen-update-price/admin/language/ru-ru/extension/module/gen_update_price.php
+++ in progress/gen-update-price/admin/language/ru-ru/extension/module/gen_update_price.php
@@ -15,10 +15,16 @@ $_['text_number']   = 'число';
 $_['entry_status']     = 'Статус';
 $_['entry_reset']     = 'Вернуть первоначальные цены';
 $_['entry_formula']   = 'Выберите операцию';
+$_['entry_update'] = 'Обновить базовые цены на текущие';
 
+$_['button_update'] = 'Обновить';
 $_['button_apply']     = 'Применить';
 
 $_['button_reset']   = 'Сброс';
 
 
 $_['error_block']   = 'Неудалось обновить цены';
+$_['column_name'] = 'Название';
+$_['column_model'] = 'Модель';
+$_['column_price'] = 'Цена';
+$_['column_status'] = 'Статус';
